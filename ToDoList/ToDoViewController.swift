5b67df9615ec86be4ada71b15dc2086f6425c7c8
Anthony Torres <antho@Anthonys-MacBook-Air-2.local>
2019-05-28 00:38:32 -0500

    - added to do view controller - added to do tabel view controller

---
+++ ToDoList/ToDoViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ToDoViewController.swift
+//  ToDoList
+//
+//  Created by Anthony Torres on 5/27/19.
+//  Copyright © 2019 Anthony Torres. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UIViewController {
+
+    @IBOutlet weak var toDoTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .blue
+        
+        let saveButton = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveToDoItem))
+        navigationItem.rightBarButtonItem = saveButton
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func saveToDoItem() {
+        print("Saved")
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
